8a3b3f9cec112d39ccf41c01adaeef7bc45a9ae2
Hugo <hugo.matijascic@polymtl.ca>
2024-09-21 22:30:02 -0400

    add custom commit message

--- scripts/save_backup_to_remote.sh
+++ scripts/save_backup_to_remote.sh
@@ -3,6 +3,13 @@ CONFIGS_BACKUP=${BACKUP_DIR}/configs
 TEMPLATES_BACKUP=${BACKUP_DIR}/templates
 SCRIPTS_BACKUP=${BACKUP_DIR}/scripts
 
+if [ $# -eq 0 ]; then
+    COMMIT_MSG="No arguments supplied"
+else
+    COMMIT_MSG=$1
+fi
+
+
 
 cp -r ~/.config/nvim ${CONFIGS_BACKUP}
 cp -r ~/.config/hypr ${CONFIGS_BACKUP}
@@ -14,5 +21,5 @@ cp -r ~/scripts/* ${SCRIPTS_BACKUP}
 
 cd ~/backup
 git add .
-git commit -m "auto save"
+git commit -m "${COMMIT_MSG}"
 git push
