5375c654b6ff8c97cc3d288bf1571f09d8f50dd1
Hugo <hugo.matijascic@polymtl.ca>
2024-09-21 22:43:23 -0400

    enhanced c template build script

--- templates/c_template/build.sh
+++ templates/c_template/build.sh
@@ -1,20 +1,23 @@
 EXE_FILENAME=template
 
-cd bin/debug && make clean
-cmake ../.. -DCMAKE_BUILD_TYPE=Debug && cmake --build . && make 
-cd .. 
-
-cd release && make clean
-cmake ../.. -DCMAKE_BUILD_TYPE=Release && cmake --build . && make
-cd ../.. 
-
-rm -rf ./compile_commands.json
-ln -s ./bin/release/compile_commands.json ./
 
 if [[ $1 == "-d" ]]; then
-    echo "Running Debug Build..."
+    cd bin/debug && make clean
+    cmake ../.. -DCMAKE_BUILD_TYPE=Debug && cmake --build . && make 
+    cd ../..
+    rm -rf ./compile_commands.json
+    ln -s ./bin/debug/compile_commands.json ./
+     echo "Running Debug Build..."
     ./bin/debug/${EXE_FILENAME}_d
 elif [[ $1 == "-r" ]]; then
+    cd bin/release && make clean
+    cmake ../.. -DCMAKE_BUILD_TYPE=Release && cmake --build . && make
+    cd ../..
+    rm -rf ./compile_commands.json
+    ln -s ./bin/release/compile_commands.json ./
     echo "Running Release Build..."
     ./bin/release/${EXE_FILENAME}
+else
+    echo "No Build Configuration specified..."
 fi
+
